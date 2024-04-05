7d16a886db1fbd72d8dccde0f5602837a7c57d05
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-04-05 15:25:33 +0700

    show details user

--- resources/js/Pages/Employee/Show.jsx
+++ resources/js/Pages/Employee/Show.jsx
@@ -18,14 +18,14 @@ const Show = ({ auth, employee }) => {
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
                             <div className="grid gap-1 grid-cols-2 mt-2">
-                                <div>
+                                <div className="grid-cols-3">
                                     <div>
                                         <label className="font-bold text-lg">Employee ID</label>
                                         <p className="mt-1">{employee.id}</p>
                                     </div>
                                     <div className="mt-4">
-                                        <label className="font-bold text-lg">Employee Name</label>
-                                        <p className="mt-1">{employee.name}</p>
+                                        <label className="font-bold text-lg">Employee User Name</label>
+                                        <p className="mt-1">{employee.user.name}</p>
                                     </div>
 
                                     <div className="mt-4">
@@ -34,29 +34,55 @@ const Show = ({ auth, employee }) => {
                                     </div>
 
                                     <div className="mt-4">
+                                        <label className="font-bold text-lg">Employee Address</label>
+                                        <p className="mt-1">{employee.address}</p>
+                                    </div>
+
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">Employee Country</label>
+                                        <p className="mt-1">{employee.country}</p>
+                                    </div>
+                                </div>
+                                <div className="grid-cols-3">
+                                    <div>
+                                        <label className="font-bold text-lg">Employee Full Name</label>
+                                        <p className="mt-1">{employee.name}</p>
+                                    </div>
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">Employee Age</label>
+                                        <p className="mt-1">{employee.age}</p>
+                                    </div>
+                                    <div>
+                                        <label className="font-bold text-lg">Employee Phone</label>
+                                        <p className="mt-1">{employee.phone}</p>
+                                    </div>
+
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">Employee City</label>
+                                        <p className="mt-1">{employee.city}</p>
+                                    </div>
+
+                                    <div className="mt-4">
+                                        <label className="font-bold text-lg">Updated At</label>
+                                        <p className="mt-1">{new Date(employee.updated_at).toISOString().slice(0, 10)}</p>
+                                    </div>
+
+                                </div>
+
+                                <div className="grid-cols-3">
+
+
+                                    <div className="mt-4">
                                         <label className="font-bold text-lg">Created At</label>
                                         <p className="mt-1">{new Date(employee.created_at).toISOString().slice(0, 10)}</p>
                                     </div>
+
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
-            {/* <div className="pb-12">
-        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div className="p-6 text-gray-900 dark:text-gray-100">
-              <TasksTable
-                tasks={tasks}
-                queryParams={queryParams}
-                hideUserColumn={true}
-              />
-            </div>
-          </div>
-        </div>
-      </div> */}
         </AuthenticatedLayout>
     );
 };
