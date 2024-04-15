625d8e7baa83c4ef39b8252649d5802c1ac26fea
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-04-15 16:50:57 +0700

    authorization with policy laravel

---
+++ laravel/abeta02-laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (auth()->attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+        ]);
+
+        $user = User::query()->create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        Auth::login($user);
+        return redirect()->route('dashboard');
+    }
+}
--- laravel/abeta02-laravel/app/Http/Controllers/UserController.php
+++ laravel/abeta02-laravel/app/Http/Controllers/UserController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Policies\UserPolicy;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->authorize(UserPolicy::class);
+    }
+
     public function index(Request $request)
     {
         $users = User::query()->orderBy('name')->paginate(5);
