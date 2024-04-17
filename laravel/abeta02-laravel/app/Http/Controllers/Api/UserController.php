05cd31e135be1188457934eaf1da0eda3ec9891c
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-04-17 15:24:56 +0700

    crud user api

---
+++ laravel/abeta02-laravel/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::query()->paginate(10);
+        return UserResource::collection($users)
+            ->additional([
+                'message' => 'Users retrieved successfully'
+            ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserRequest $request)
+    {
+        $data = $request->validated();
+        $data['password'] = bcrypt($data['password']);
+        $user = User::query()->create($data);
+        return response()->json([
+            'message' => 'User created successfully',
+            'data' => new UserResource($user)
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        $data = $request->only(['name', 'email']);
+        $user->update($data);
+        return response()->json([
+            'message' => 'User updated successfully',
+            'data' => new UserResource($user)
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->json([
+            'message' => 'User deleted successfully'
+        ]);
+    }
+}
