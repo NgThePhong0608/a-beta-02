8215c2e16fed07bd69251e103e37a79c38c50d79
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-05-06 09:49:38 +0700

    update avatar path

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -75,7 +75,9 @@ export default function Authenticated({ user, header, children }) {
                                                     <div className="text-xs text-gray-500">{user.email}</div>
                                                 </div>
                                                 <img className="w-10 h-10 rounded-full m-4 object-cover"
-                                                    src={REACT_APP_BASE_URL + "/storage/" + user.avatar}
+                                                    // src={REACT_APP_BASE_URL + "/storage/" + user.avatar}
+                                                    src={user.avatar != null ? REACT_APP_BASE_URL + "/storage/" + user.avatar :
+                                                        "https://st3.depositphotos.com/9998432/13335/v/450/depositphotos_133352156-stock-illustration-default-placeholder-profile-icon.jpg"}
                                                     alt="" />
                                                 <svg
                                                     className="ms-2 -me-0.5 h-4 w-4"
