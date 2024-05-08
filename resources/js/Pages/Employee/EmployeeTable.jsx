f4bb562225327069a5c95a6e7e3b7e3a2775b6d1
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-05-08 08:55:58 +0700

    chore: Refactor employee show page layout and navigation

--- resources/js/Pages/Employee/EmployeeTable.jsx
+++ resources/js/Pages/Employee/EmployeeTable.jsx
@@ -9,7 +9,8 @@ import { REACT_APP_BASE_URL } from "@/constant";
 const EmployeeTable = ({ auth, employees, queryParams = null, success }) => {
     queryParams = queryParams || {};
     useEffect(() => {
-        console.log(employees.data[0].user)
+        console.log(employees.data[0])
+        console.log(auth.user);
     }, []);
     const [searchQuery, setSearchQuery] = useState('');
     const searchFieldChanged = (name, value) => {
@@ -63,6 +64,11 @@ const EmployeeTable = ({ auth, employees, queryParams = null, success }) => {
         }
         router.put(route("employee.reset-password", employee.id));
     }
+
+    const checkPermission = (employee) => {
+        return auth.user.id === employee.user.id;
+    }
+
     return (
         <>
             {success && (
@@ -143,6 +149,11 @@ const EmployeeTable = ({ auth, employees, queryParams = null, success }) => {
                                 Create Date
                             </TableHeading>
                             {checkRole(auth) && (
+                                <th className="px-3 py-2 text-center">
+                                    Actions
+                                </th>
+                            )}
+                            {checkPermission(auth) && (
                                 <th className="px-3 py-2 text-center">
                                     Actions
                                 </th>
@@ -229,7 +240,7 @@ const EmployeeTable = ({ auth, employees, queryParams = null, success }) => {
                                                     href={route("employee.edit", employee.id)}
                                                     className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
                                                 >
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                                     </svg>
                                                 </Link>
@@ -237,7 +248,7 @@ const EmployeeTable = ({ auth, employees, queryParams = null, success }) => {
                                                     onClick={(e) => deleteEmployee(employee)}
                                                     className="font-medium text-red-600 dark:text-red-500 hover:underline"
                                                 >
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                     </svg>
                                                 </button>
@@ -245,8 +256,32 @@ const EmployeeTable = ({ auth, employees, queryParams = null, success }) => {
                                                     onClick={(e) => handleResetPassword(employee)}
                                                     className="font-medium hover:underline"
                                                 >
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3" />
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3" />
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                        </td>
+                                    )
+                                }
+                                {
+                                    checkPermission(employee) && (
+                                        <td className="px-3 py-2 text-nowrap">
+                                            <div className="flex items-center space-x-1">
+                                                <Link
+                                                    href={route("employee.edit", employee.id)}
+                                                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                                                >
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                                        <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                                    </svg>
+                                                </Link>
+                                                <button
+                                                    onClick={(e) => deleteEmployee(employee)}
+                                                    className="font-medium text-red-600 dark:text-red-500 hover:underline"
+                                                >
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                                        <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                                     </svg>
                                                 </button>
                                             </div>
