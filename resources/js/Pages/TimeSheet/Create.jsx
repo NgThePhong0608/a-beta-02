cbc29e1c8e2576ac3f809bc4d6cce0d1e60211e0
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-05-08 08:55:41 +0700

    chore: Fix formatting in TimeSheet Edit and Create components

--- resources/js/Pages/TimeSheet/Create.jsx
+++ resources/js/Pages/TimeSheet/Create.jsx
@@ -1,8 +1,8 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.jsx";
-import {Head} from "@inertiajs/react";
+import { Head } from "@inertiajs/react";
 import React from "react";
 
-const Create = ({auth}) => {
+const Create = ({ auth }) => {
 
     return (
         <AuthenticatedLayout
@@ -28,12 +28,12 @@ const Create = ({auth}) => {
                                     enterprise technology acquisitions of 2021 so far, in reverse chronological
                                     order.</p>
                                 <a href="#"
-                                   className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     Checkin
                                     <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
-                                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                              stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                            strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                                     </svg>
                                 </a>
                             </div>
