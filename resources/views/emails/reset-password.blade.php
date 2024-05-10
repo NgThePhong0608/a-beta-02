a0751aa14f4c13f41b0c0a3ec4fccd4aaf44268e
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-05-10 10:02:37 +0700

    chore: Update email templates for user verification and password reset

--- resources/views/emails/reset-password.blade.php
+++ resources/views/emails/reset-password.blade.php
@@ -6,11 +6,40 @@
 </head>
 
 <body>
-    <h2>Dear {{ $user['name'] }}</h2>
-    <br />
-    Your password has been reset to default by admin. You can login to system now.
-    <br />
-    <a href="{{ url('login') }}">Login here !</a>
+    <section class="max-w-2xl px-6 py-8 mx-auto bg-white dark:bg-gray-900">
+        <header>
+            <a href="{{ route('login') }}">
+                <img class="w-auto h-7 sm:h-8" src="http://localhost:8000/storage/hrm.png" alt="">
+            </a>
+        </header>
+
+        <main class="mt-8">
+            <h2 class="text-gray-700 dark:text-gray-200">Hi {{ $user->name }},</h2>
+
+            <p class="mt-2 leading-loose text-gray-600 dark:text-gray-300">
+                Your password has been reset to default by admin. You can login to system now.
+            </p>
+
+            <a class="px-6 py-2 mt-4 text-sm font-medium tracking-wider text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+                href="{{ url('login') }}">Login here !</a>
+
+            <p class="mt-8 text-gray-600 dark:text-gray-300">
+                Thanks, <br>
+                HRM team
+            </p>
+        </main>
+
+
+        <footer class="mt-8">
+            <p class="text-gray-500 dark:text-gray-400">
+                This email was sent from <a href="#" class="text-blue-600 hover:underline dark:text-blue-400"
+                    target="_blank">hrm@hrm.com</a>.
+            </p>
+
+            <p class="mt-3 text-gray-500 dark:text-gray-400">© <?php echo date('Y'); ?> HRM System. All Rights
+                Reserved.</p>
+        </footer>
+    </section>
 </body>
 
 </html>
