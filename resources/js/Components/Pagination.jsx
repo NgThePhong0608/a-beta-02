4337cfd8591d86f02c2fcc4efd4c280f9386fee3
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-05-13 16:09:10 +0700

    Refactor Pagination component to use consistent text color and hover styles

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -3,17 +3,17 @@ import React from "react";
 
 const Pagination = ({ links }) => {
     return (
-        <nav className="text-center mt-4">
+        <nav className="text-center m-4">
             {links.map((link, index) => (
                 <Link
                     key={index}
                     href={link.url}
                     className={
-                        "inline-block py-2 px-3 rounded-lg text-gray-500 text-xs " +
+                        "inline-block py-2 px-3 rounded-lg text-black-500 text-xs " +
                         (link.active ? "bg-blue-500 " : " ") +
                         (!link.url
                             ? "!text-gray-500 cursor-not-allowed "
-                            : "hover:bg-gray-700")
+                            : "hover:bg-gray-100 hover:text-gray-700")
                     }
                 >
                     <span dangerouslySetInnerHTML={{ __html: link.label }}></span>
